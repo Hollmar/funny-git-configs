562f17f2301f13a7abdd437749ba60faf7bea588
Filip Brkic <filipb@gmx.net>
2024-05-23 00:33:47 +0200

    Added german version for military git aliases

---
+++ militarygit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git config --local alias.errichte init
+
+git config --local alias.einsatzbericht log
+
+git config --local alias.bereitmachen add
+
+git config --local alias.feuer commit 
+
+git config --local alias.marsch push
+
+git config --local alias.buendnis merge
+
+git config --local alias.imitieren clone 
+
+git config --local alias.division branch
+
+git config --local alias.verstaerkung pull
+
+git config --local alias.neuformation rebase
+
+git config --local alias.spaehertrupp diff
+
+git config --local alias.auslagern checkout 
+
+git config --local alias.verrat blame
+
+git config --local alias.lagebericht status 
+
+git config --local alias.bunkern stash
+
+git config --local alias.erkennungsmarke tag
